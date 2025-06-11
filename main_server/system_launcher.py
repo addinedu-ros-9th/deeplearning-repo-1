a6e89020da35ff65b5d5cf3b9d829d69988165fc
leesh0806 <leesh20806@gmail.com>
2025-06-11 16:21:46 +0900

    update

--- main_server/system_launcher.py
+++
@@ -1,8 +0,0 @@
-# main_server/system_launcher.py
-from .system_manager import SystemManager
-
-if __name__ == '__main__':
-    server = SystemManager()
-    server.start()
-
-# python -m main_server.system_launcher
--- robot_app/config.py
+++
@@ -1,6 +0,0 @@
-# robot_app/config.py
-MAIN_SERVER_IP = "192.168.45.238"  # <<< 메인 서버 PC의 IP 주소로 반드시 변경!!!
-VIDEO_PORT = 9998                # 영상 전송 UDP 포트
-COMMAND_PORT = 9996              # 명령 수신 TCP 포트
-CAMERA_INDEX = 0
-JPEG_QUALITY = 50
